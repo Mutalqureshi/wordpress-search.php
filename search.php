d4cbef503981d5a7e85ab846658476d5e064c93b
Mutalqureshi <35372795+Mutalqureshi@users.noreply.github.com>
2020-03-04 15:25:30 +0500

    Add files via upload

--- search.php
+++ search.php
@@ -1,47 +1,140 @@
-<?php echo get_header(); ?>
-         
-<div id="primary" class="content-area">
-    <main id="main" class="site-main container-fluid p-none">
-        <div class="col-background blog-content">
+<?php
+/*
+Template Name: Search Page
+*/
+get_header(); ?>
+	<section id="primary <?php echo "default page-".$post_id = get_the_ID();?>" class="all-pages <?php echo "page-".$post_id = get_the_ID(); wp_title('');?>">
+		<div class="other-page-title">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<h1 class="page-title"></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<section class="search-all">
+			<div class="container">
+					<?php
+					if ( have_posts() ) : ?>
+					
+			<?php 		
+					
+					//echo "searching page";
+					/*-----this is for pages----------*/
+					?>
+                    <h1 class="search-head"><?php 	printf( esc_html__( 'Results for: %s', 'storevilla' ), '<span>' . get_search_query() . '</span>' );  //the_title(); ?></h1>
+				<!-- row-->	
+<section class="all-blog">
+
+                <div class="row">
+                    <div class="col-md-8">
+                            <?php
+                            /* Start the Loop */
+                            if(have_posts()){
+                                while ( have_posts() ) : the_post();
+                            ?>
     
-            <h3 class="page-title">Search Results for &ldquo;<?=get_search_query()?>&rdquo;</h3>
-                    <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
-                    <div class="blogBox clearfix">
-                        <h6><strong><a href="<?php the_permalink(); ?>"><?php the_title();?></a></strong></h6>
+                <div class="blog-detail-content">
+                    <div class="blog-img">
+                           <div class="zoom">
+                               <?php if (has_post_thumbnail($post->ID)) { ?>
+                                    <?php $url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'gutsy_blog'); ?>
+                                         <a href="<?= the_permalink(); ?>">
+                                    <img src="<?php echo $url[0]; ?>" class="img-fluid">
+                                         </a>
+                                <?php } else { ?>
+                                    <img src="http://placehold.it/900x308" width="900" height="308" class="img-fluid">
+                                <?php } ?>
+                            <div class="date-blog">
+                                <?php echo get_the_time('M');?>
+                                <hr class="blog-hr"><span class="max-date"><?php echo get_the_time('d');?></span>
+                                <hr class="blog-hr"><?php echo get_the_time('Y');?>
+                            </div>
+                           </div>
+                    </div>
+
+                        <h3><a href="<?= the_permalink(); ?>" class="gutsy_blog_title"><?php the_title(); ?> </a></h3>
+                                    <div class="main-blogDesc">
+                                                    <p><?php echo get_my_excerpt(45) ?></p>
+                                                    <div class="more_link">
+                                                        <a href="<?= the_permalink() ?>" class="readmore">Read More...</a>
+                                                    </div>
+                                                    <div class="blogListFooter">
+                                                        <p>
+                                                            <!-- <span class="listAuthor"><?php echo get_the_date(); ?> By <?php the_author_link(); ?></span> -->
+                                                            <span class="listAuthor"><i class="far fa-clock"></i><?php echo get_the_date(); ?> By <?php the_author_link(); ?></span>
+                                                            <!-- <span class="listTime">in Our Services</span> -->
+                                                            <!-- <span class="listTime">in <?php //the_category(', '); ?></span> -->
+                                                        </p>
+                                                    </div><!-- ( BLOG LIST FOOTER END ) -->
                                              
-                         <div class="blogDesc">
-                            <p>
-                                <?php the_excerpt() ?>
-                            </p>
-                        </div>    
-                                                
-                        <!-- ( BLOG DESCRIPTION END ) -->
-                    </div><!-- ( BLOG BOX END ) -->
-                    <?php endwhile; ?>
-            
-                    <?php else: ?>
-                        <?php echo "No result found."; //get_template_part( 'content', 'none' ); ?>
-                    <?php endif; ?>
+
+                                                <div class="shareField">
+                                                        <?php echo do_shortcode('[Social9_Share]'); ?>
+                                                </div>
+
+                                    </div><!--blog desc-->
+                           
 
 
-<!-- pagination -->
-        <?php if ( have_posts() ) : ?>
-        <!-- Add the pagination functions here. -->
-        <!-- Start of the main loop. -->
-        <?php while ( have_posts() ) : the_post();  ?>
-        <!-- the rest of your theme's main loop -->
-        <?php endwhile; ?>
-        <!-- End of the main loop -->
-        <!-- Add the pagination functions here. -->
-        <div class="nav-previous"><?php previous_posts_link( '< Previous Page' ); ?></div>
-        <div class="nav-next"><?php next_posts_link( 'Next Page >' ); ?></div>
+                    </div>    
+                            <?php endwhile;
 
+                             ?>
+                        <div class="pagination-sec">
+                        <?php /*custom added*/
+                        wpbeginner_numeric_posts_nav(); 
+                          ?>
+                        </div> 
+                       <?php } ?>
+                    </div>
         
-        <?php endif; ?>
-<!-- pagination -->
-<br>
-        </div><!--col-back ground-->
-    </main><!-- #main -->
-</div>
+        <div class="col-lg-4">
+            <?php dynamic_sidebar( 'blog-sidebar' ); ?>
+        </div>
+                    </div>  
+   
+        </section>
+	
+					
 
-<?php echo get_footer(); ?>
+
+					
+				<?php	
+				/*-----this is for pages-ended---------*/
+		
+		
+
+?>		
+
+
+				
+				
+				
+				
+				
+				
+					<?php	
+					// the_posts_pagination();
+					else :
+					//get_template_part( 'template-parts/content', 'none' );
+					?>	
+					<section class="no-result-post not-found ">
+						<div class="container">
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="page-content text-center">
+										<p class="not-found-para"><?php _e( 'It looks like nothing was found ? Search an other', '' ); ?></p>
+										<?php get_search_form(); ?>
+									</div><!-- .page-content -->
+								</div>
+							</div>
+						</div>
+					</section>
+						<?php	
+					endif; ?>
+			</div>
+		</section>
+	</section>
+<?php get_footer();
---
+++ searchform.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying search forms
+ *
+ */	
+	
+	?>
+<form role="search" method="get" id="searchform"
+    class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <div>
+        <label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
+		<input type="hidden" name="post_type" value="post">
+        <input type="text" placeholder="Search" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+        <input type="submit" id="searchsubmit"
+            value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+    </div>
+</form>
